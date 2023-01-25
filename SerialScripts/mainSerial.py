e873cb7ce37ec52c387f2cdad9974daa782c0717
Jero Vargas <jeronimo@focus.uy>
2023-01-25 09:39:10 -0300

    Add necessary functions to serial

--- SerialScripts/mainSerial.py
+++ SerialScripts/mainSerial.py
@@ -14,68 +14,86 @@ from windows_com_find.port_find import get_port_name, print_available_ports
     #                     *args, 
     #                     **kwargs)
 
-_ser = rs485.RS485_serial(19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
-_ser.set_port(get_port_name('USB-SERIAL CH340'))
-_ser.open()
+serial_interface_name = 'USB-SERIAL CH340'
 
-tx_buffer = bytearray()
-rx_buffer = bytearray()
-
-def send_read_cmd():
-    print('sending READ command ...')
+class IPG_status():
+    def __init__(self):
+        self.burnin_active = False
+        self.stimulating = False
+        self.communicating = False
     
-    # tx_buffer.append(0xAA)
-    # tx_buffer.append(0xCC)
-    # _ser.write(tx_buffer)
-    # tx_buffer.extend(0xAA,0xBB)
-    # tx_buffer.append(0x02)
+    def parse_byte(self,  byte):
+        self.stimulating =  True if byte & 0b00001000 != 0 else False
+        self.communicating =  True if byte & 0b00000100 != 0 else False
 
+class IPG():
+    def __init__(self, id):
+        self.idNum = id
+        self.status = IPG_status()
 
-def send_start_burn_in_cmd():
-    print('sending START command ...')
-    tx_buffer.clear()
-    tx_buffer.extend(0xAA,0xBB)
-    # tx_buffer.append(0x02)
-    # _ser.write(0xAA)
-    # _ser.write(0xBB)
+class BurnInBoard():
+
+    IPG_in_board = 2
     
-def print_read_cmd_response():
-    print('waiting for response...')
-    
+    def __init__(self):
+        self.IPGList = [IPG(id) for id in range(IPG_in_board)]
+        
+    def set_IPG_status(self, ipg_num : int, ipg_st : IPG_status):
+        self.IPGList[ipg_num].status = ipg_st
 
+    def get_IPG_status(self, ipg_num : int) ->  IPG_status:
+        return self.IPGList[ipg_num]
 
-# compu deberia
-# preguntar por info
-# Esperar 
-#imprimir la info
+class Serial():
 
+    def __init__(self, *args):
+        self._tx_buffer = bytearray()
+        self._rx_buffer = bytearray()
+        self._ser = rs485.RS485_serial(19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+        self._ser.set_port(get_port_name(serial_interface_name))
+        self._ser.open()
+        
+    def command_get_status(self,Board_num : int):
+        self._tx_buffer.clear()
+        self._tx_buffer.append(0xAA)
+        self._tx_buffer.append(0x01)
+        self._ser.write(self._tx_buffer)
+        self._ser.flushOutput()
+        return ' '.join(hex(byte) for byte in bytes(self._ser.read(2)))
 
-num = 0
+    def command_start_burn_in(self):
+        self._tx_buffer.clear()
+        self._tx_buffer.append(0xAA)
+        self._tx_buffer.append(0x02)
+        self._ser.write(self._tx_buffer)
+        self._ser.flushInput()
+        self._ser.flushOutput()
+
+    def command_stop_burn_in(self):
+        self._tx_buffer.clear()
+        self._tx_buffer.append(0xAA)
+        self._tx_buffer.append(0x03)
+        self._ser.write(self._tx_buffer)
+        self._ser.flushInput()
+        self._ser.flushOutput()
+
 if __name__ == "__main__":
+    ser = Serial()
     while True:
         # send_read_cmd()
         # _ser.write(tx_buffer)
         option = input('OPCIONES:\n  (1) get status\n  (2) start burnin\n  (3) stop burnin\n    -> ')
         if option == '1':
-            tx_buffer.clear()
-            tx_buffer.append(0xAA)
-            tx_buffer.append(0x01)
-            _ser.write(tx_buffer)
+            ser.command_get_status()
             print('Status')
             print(' '.join(hex(byte) for byte in bytes(_ser.read(2))))
             print('')
         elif option == '2':
-            tx_buffer.clear()
-            tx_buffer.append(0xAA)
-            tx_buffer.append(0x02)
-            _ser.write(tx_buffer)
+            ser.command_start_burn_in()
             print('Sending START command')
             print('')
         elif option == '3':
-            tx_buffer.clear()
-            tx_buffer.append(0xAA)
-            tx_buffer.append(0x03)
-            _ser.write(tx_buffer)
+            ser.command_stop_burn_in()
             print('Sending STOP command')
             print('')
         else:
@@ -84,15 +102,3 @@ if __name__ == "__main__":
         time.sleep(1)
         _ser.flushInput()
         _ser.flushOutput()
-        # send_start_burn_in_cmd()
-        # print('going to SLEEP')
-        # time.sleep(10)
-# if __name__ == "__main__":      
-#     print_available_ports()
-#     for i in range(255):
-#         _ser.write(buffer)
-#         buffer[0] = buffer[0] + 1
-#         buff = _ser.read()
-#         print(buff)
-#         time.sleep(0.02)
-    
